c3e4519fe7cd3cadc4728336fe714f0feaa6cb34
Xojiakbar994 <khojiakbartulkinov@gmail.com>
2023-08-01 06:01:38 +0500

    fix answer value to list

--- 2469_Convert the Temperature.py
+++ 2469_Convert the Temperature.py
@@ -1,6 +1,6 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> list[float]:
-        return(celsius+273.15, celsius * 1.80 + 32.00)
+        return[celsius+273.15, celsius * 1.80 + 32.00]
     
 a = Solution()
 print(a.convertTemperature(36.50))
