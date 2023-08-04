a61cfd45d2f4a0e0054b4c4ef8311ffb2b336832
Xojiakbar994 <khojiakbartulkinov@gmail.com>
2023-08-04 08:13:17 +0500

    258 solved

--- 258. Add Digits.py
+++ 258. Add Digits.py
@@ -1,5 +1,9 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        while num > 9:
+            num = num % 10 + num // 10
+        return num
 
 a = Solution()
-print(a.sol)
+print(a.addDigits(38)) #2
+print(a.addDigits(0))  #0
