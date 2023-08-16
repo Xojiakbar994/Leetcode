6cf9eb0d36a320c1e628a900d54c2c9de6a525e7
Xojiakbar994 <khojiakbartulkinov@gmail.com>
2023-08-16 21:11:34 +0500

    1689 solved

---
+++ medium/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
+
+a = Solution()
+
+print(a.minPartitions(n = "32"))
+print(a.minPartitions(n = "82734"))
+print(a.minPartitions(n = "27346209830709182346"))
