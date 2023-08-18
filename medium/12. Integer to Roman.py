60c393d4953c4fc4b17b4ec305021a9ea4196e8f
Xojiakbar994 <khojiakbartulkinov@gmail.com>
2023-08-18 20:17:54 +0500

    create 1222

--- medium/12. Integer to Roman.py
+++ medium/12. Integer to Roman.py
@@ -23,7 +23,7 @@ class Solution:
         C = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
         X = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
         I = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
-        return M[num//1000] + C[num%1000//100] + X[num%1000%100//10] + I[num%1000%100%10]
+        return M[num//1000] + C[num%1000//100] + X[num%100//10] + I[num%10]
     
 a = Solution()
 print(a.intToRoman(3))
---
+++ medium/1222. Queens That Can Attack the King.py
@@ -0,0 +1,8 @@
+class Solution:
+    def queensAttacktheKing(self, queens: list[list[int]], king: list[int]) -> list[list[int]]:
+        pass
+
+
+a = Solution()
+print(a.queensAttacktheKing(queens = [[0,1],[1,0],[4,0],[0,4],[3,3],[2,4]], king = [0,0]))
+print(a.queensAttacktheKing(queens = [[0,0],[1,1],[2,2],[3,4],[3,5],[4,4],[4,5]], king = [3,3]))
